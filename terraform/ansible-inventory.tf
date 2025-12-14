d6f780d76e50ba68140e8fb073763b58da62dc93
Jeremy Tomasi <me@jeremytomasi.fr>
2025-12-14 17:31:44 +0100

    (terraform): mail server added

--- terraform/ansible-inventory.tf
+++ terraform/ansible-inventory.tf
@@ -8,8 +8,18 @@ resource "ansible_host" "SRV-01" {
   }
 }
 
+resource "ansible_host" "MAIL-01" {
+  name = proxmox_virtual_environment_vm.MAIL-01.name
+  groups = ["servers"]
+  variables = {
+    ansible_user = "jeremy"
+    ansible_ssh_private_key_file = var.ssh_private_key
+    ansible_host = join("",proxmox_virtual_environment_vm.MAIL-01.ipv4_addresses[2])
+  }
+}
+
 resource "ansible_host" "STORAGE-01" {
-  name = "Monaco"
+  name = "STORAGE-01"
   groups = ["servers"]
   variables = {
     ansible_host = "192.168.1.253"
--- terraform/infra.tf
+++ terraform/infra.tf
@@ -59,6 +59,67 @@ resource "proxmox_virtual_environment_vm" "SRV-01" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "MAIL-01" {
+  name = "MAIL-01"
+  description = "VM for Mail server"
+  tags = ["linux","server","prod"]
+  node_name = "LeMans"
+
+  agent {
+    enabled = true
+  }
+
+  network_device {
+    bridge = "vmbr2"
+    vlan_id = 10
+    model = "e1000"
+  }
+
+  network_device {
+    bridge = "vmbr2"
+    vlan_id = 99
+    model = "e1000"
+  }
+
+  initialization {
+    datastore_id = "local"
+
+    dns {
+      domain = "jeremytomasi.fr"
+      servers = ["192.168.10.254"]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.10.251/24"
+        gateway = "192.168.10.254"
+      }
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.99.251/24"
+        gateway = "192.168.99.254"
+      }
+    }
+
+    user_account {
+      username = "jeremy"
+      keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE8qscFPE4POlT4WX0Ju1JmDDc0GhL5yp56z/+Tc7u/8 ansible_config",
+        "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIC32RZaHUzt2hlELKu7qAb0PPKnnJFHyvDHMWnhjO9l3AAAABHNzaDo= me@jeremytomasi.fr"
+      ]
+    }
+  }
+
+  clone {
+    vm_id = 101
+    datastore_id = "local"
+    full = true
+    node_name = "LeMans"
+  }
+}
+
 resource "proxmox_virtual_environment_pool" "PROD" {
   pool_id = "PROD"
 }
@@ -71,4 +132,9 @@ resource "proxmox_virtual_environment_pool_membership" "FW-01" {
 resource "proxmox_virtual_environment_pool_membership" "SRV-01" {
   pool_id = proxmox_virtual_environment_pool.PROD.id
   vm_id = proxmox_virtual_environment_vm.SRV-01.id
+}
+
+resource "proxmox_virtual_environment_pool_membership" "MAIL-01" {
+  pool_id = proxmox_virtual_environment_pool.PROD.id
+  vm_id = proxmox_virtual_environment_vm.MAIL-01.id
 }
