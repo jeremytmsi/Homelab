a233933e614ceb84cb0bc5cb8d0e4baa3bb3400d
Jeremy Tomasi <me@jeremytomasi.fr>
2025-12-31 00:35:56 +0100

    (terraform): qual infrastructure refactoring

--- terraform/ansible-inventory-qual.tf
+++ terraform/ansible-inventory-qual.tf
@@ -1,20 +1,10 @@
 resource "ansible_host" "SRV-QUAL-01" {
   name = proxmox_virtual_environment_vm.SRV-QUAL-01.name
-  groups = ["services","qual","docker"]
+  groups = ["qual","docker"]
   variables = {
     ansible_user = "ansible"
     ansible_ssh_private_key_file = var.ssh_private_key
     ansible_host = join("",proxmox_virtual_environment_vm.SRV-QUAL-01.ipv4_addresses[2])
-  }
-}
-
-resource "ansible_host" "MAIL-QUAL-01" {
-  name = proxmox_virtual_environment_vm.MAIL-QUAL-01.name
-  groups = ["mail","qual"]
-  variables = {
-    ansible_user = "ansible"
-    ansible_ssh_private_key_file = var.ssh_private_key
-    ansible_host = join("",proxmox_virtual_environment_vm.MAIL-QUAL-01.ipv4_addresses[2])
   }
 }
 
