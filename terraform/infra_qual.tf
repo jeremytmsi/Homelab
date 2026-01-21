bd0852a9abbe9c08f5b521ce8dd07ceffc9efbdc
Jeremy Tomasi <me@jeremytomasi.fr>
2026-01-21 20:29:34 +0100

    (terraform): mail qual server added

--- terraform/infra_qual.tf
+++ terraform/infra_qual.tf
@@ -4,7 +4,7 @@ resource "proxmox_virtual_environment_vm" "DOCKER-QUAL-01" {
   tags = ["linux","server","qual"]
   node_name = var.node_name
   stop_on_destroy = true
-  vm_id = 112
+  vm_id = 110
 
   agent {
     enabled = true
@@ -65,13 +65,76 @@ resource "proxmox_virtual_environment_vm" "DOCKER-QUAL-01" {
   }
 }
 
+resource "proxmox_virtual_environment_vm" "MAIL-QUAL-01" {
+  name = "mail.qual.jeremytomasi.fr"
+  description = "VM for hosting mail server"
+  tags = ["linux","server","qual"]
+  node_name = var.node_name
+  stop_on_destroy = true
+  vm_id = 111
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = 4
+    type = "host"
+  }
+
+  network_device {
+    model = "e1000"
+    bridge = "vmbr2"
+    vlan_id = 20
+  }
+
+  network_device {
+    model = "e1000"
+    bridge = "vmbr2"
+    vlan_id = 98
+  }
+
+  initialization {
+    datastore_id = "local"
+
+    dns {
+      domain = "qual.jeremytomasi.fr"
+      servers = ["192.168.20.254"]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.20.252/24"
+        gateway = "192.168.20.254"
+      }
+    }
+
+    ip_config {
+      ipv4 {
+        address = "192.168.98.252/24"
+        gateway = "192.168.98.254"
+      }
+    }
+
+    user_account {
+      username = var.vm_ssh_user
+      keys = var.vm_ssh_keys
+    }
+  }
+
+  clone {
+    vm_id = 190
+    full = true
+  }
+}
+
 resource "proxmox_virtual_environment_vm" "MONITORING-QUAL-01" {
   name = "monitoring.qual.jeremytomasi.fr"
   description = "VM for monitoring"
   tags = ["linux","server","qual"]
   node_name = var.node_name
   stop_on_destroy = true
-  vm_id = 114
+  vm_id = 112
 
   agent {
     enabled = true
