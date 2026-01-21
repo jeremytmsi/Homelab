17ceefc73da92130317bfb5f0a0723d7558dd41b
Jeremy Tomasi <me@jeremytomasi.fr>
2026-01-21 14:37:29 +0100

    (terraform): Nextcloud DB VM removed

--- terraform/infra_qual.tf
+++ terraform/infra_qual.tf
@@ -123,11 +123,9 @@ resource "proxmox_virtual_environment_vm" "NEXTCLOUD-QUAL-01" {
   }
 }
 
-
-
-resource "proxmox_virtual_environment_vm" "NEXTCLOUDDB-QUAL-01" {
-  name = "nextclouddb.qual.jeremytomasi.fr"
-  description = "VM for hosting Nextcloud database"
+resource "proxmox_virtual_environment_vm" "MONITORING-QUAL-01" {
+  name = "monitoring.qual.jeremytomasi.fr"
+  description = "VM for monitoring"
   tags = ["linux","server","qual"]
   node_name = var.node_name
   stop_on_destroy = true
@@ -168,65 +166,6 @@ resource "proxmox_virtual_environment_vm" "NEXTCLOUDDB-QUAL-01" {
     ip_config {
       ipv4 {
         address = "192.168.98.250/24"
-        gateway = "192.168.98.254"
-      }
-    }
-
-    user_account {
-      username = var.vm_ssh_user
-      keys = var.vm_ssh_keys
-    }
-  }
-
-  clone {
-    vm_id = 190
-    full = true
-  }
-}
-
-resource "proxmox_virtual_environment_vm" "MONITORING-QUAL-01" {
-  name = "monitoring.qual.jeremytomasi.fr"
-  description = "VM for monitoring"
-  tags = ["linux","server","qual"]
-  node_name = var.node_name
-  stop_on_destroy = true
-  vm_id = 116
-
-  agent {
-    enabled = true
-  }
-
-  network_device {
-    model = "virtio"
-    bridge = "vmbr2"
-    vlan_id = 20
-  }
-
-  network_device {
-    model = "virtio"
-    bridge = "vmbr2"
-    vlan_id = 98
-  }
-
-  initialization {
-    datastore_id = "local"
-
-    dns {
-      domain = "qual.jeremytomasi.fr"
-      servers = ["192.168.20.254"]
-    }
-
-
-    ip_config {
-      ipv4 {
-        address = "192.168.20.249/24"
-        gateway = "192.168.20.254"
-      }
-    }
-
-    ip_config {
-      ipv4 {
-        address = "192.168.98.249/24"
         gateway = "192.168.98.254"
       }
     }
--- terraform/pool_qual.tf
+++ terraform/pool_qual.tf
@@ -12,11 +12,6 @@ resource "proxmox_virtual_environment_pool_membership" "NEXTCLOUD-QUAL-01" {
   vm_id = proxmox_virtual_environment_vm.NEXTCLOUD-QUAL-01.vm_id
 }
 
-resource "proxmox_virtual_environment_pool_membership" "NEXTCLOUDDB-QUAL-01" {
-  pool_id = proxmox_virtual_environment_pool.QUAL.id
-  vm_id = proxmox_virtual_environment_vm.NEXTCLOUDDB-QUAL-01.vm_id
-}
-
 resource "proxmox_virtual_environment_pool_membership" "MONITORING-QUAL-01" {
   pool_id = proxmox_virtual_environment_pool.QUAL.id
   vm_id = proxmox_virtual_environment_vm.MONITORING-QUAL-01.vm_id
